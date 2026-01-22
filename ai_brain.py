d896c165760e99e46c4f01159e2126838799bf1e
Vikash Singh <89346884+vikashsinghldp@users.noreply.github.com>
2026-01-22 18:06:56 +0530

    Update ai_brain.py

--- ai_brain.py
+++ ai_brain.py
@@ -1,13 +1,14 @@
 from openai import OpenAI
 
-client = OpenAI()
+client = OpenAI(os.getenv("OPENAI_API_KEY"))
 
 def ai_reply(user_text):
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-5-nano",
         messages=[
             {"role": "system", "content": open("sales_prompt.txt").read()},
             {"role": "user", "content": user_text}
         ]
     )
     return response.choices[0].message.content
+
