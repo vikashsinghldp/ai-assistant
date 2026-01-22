14726bc4c3cc7e9db0517714f0a49e5cfb582381
Vikash Singh <89346884+vikashsinghldp@users.noreply.github.com>
2026-01-22 17:54:09 +0530

    Update exotel.py

--- exotel.py
+++ exotel.py
@@ -1,10 +1,12 @@
 
 import requests
 from requests.auth import HTTPBasicAuth
+import os
 
-EXOTEL_SID = "visionsoftcloud1"
-EXOTEL_TOKEN = "273b377b26b7ccdf26e9e24d1c4c752f707cfe294de9add9"
-EXOTEL_NUMBER = "09513886363"   # your Exotel virtual number
+
+EXOTEL_SID = os.getenv("EXOTEL_SID")
+EXOTEL_TOKEN = os.getenv("EXOTEL_TOKEN")
+EXOTEL_NUMBER = os.getenv("EXOTEL_NUMBER")
 
 def make_call(to_number):
     url = f"https://api.exotel.com/v1/Accounts/{EXOTEL_SID}/Calls/connect.json"
@@ -27,4 +29,5 @@ def make_call(to_number):
     print("Response:", response.text)
 
 # 👉 CALL HERE
+
 make_call("6204529785")   # test local number
